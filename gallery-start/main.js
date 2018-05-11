7a798edfd71036a6bf616580dff1954874c54252
Apostolos Sotiropoulos <apostolis@theartwishlist.com>
2018-05-11 13:23:00 +0300

    refactor to hide global vars

--- gallery-start/main.js
+++ gallery-start/main.js
@@ -1,23 +1,18 @@
-// TODO: hide global vars
-var displayedImage = document.querySelector('.displayed-img')
-var thumbBar = document.querySelector('.thumb-bar')
-
 /* Looping through images */
 for (let i = 1; i < 6; i++) {
   let newImage = document.createElement('img')
   newImage.path = 'images/pic' + i + '.jpg'
   newImage.setAttribute('src', newImage.path)
   newImage.onclick = function () {
-    displayedImage.src = newImage.path
+    document.querySelector('.displayed-img').src = newImage.path
   }
-  thumbBar.appendChild(newImage)
+  document.querySelector('.thumb-bar').appendChild(newImage)
 }
 
 /* Wiring up the Darken/Lighten button */
-// TODO: hide global vars
-var overlay = document.querySelector('.overlay')
 var btn = document.querySelector('button')
 btn.onclick = function () {
+  let overlay = document.querySelector('.overlay')
   let overlayType = overlay.className.split(' ')[1]
   switch (overlayType) {
     case 'full-transparent':
