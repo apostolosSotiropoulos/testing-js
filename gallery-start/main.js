4c84b3fffd4a6fb5b96c17f0d00d9dfea297d9ed
Apostolos Sotiropoulos <apostolis@theartwishlist.com>
2018-05-11 12:11:10 +0300

    unexpected behaviour due to var scope

--- gallery-start/main.js
+++ gallery-start/main.js
@@ -1,14 +1,19 @@
-var displayedImage = document.querySelector('.displayed-img');
-var thumbBar = document.querySelector('.thumb-bar');
+var displayedImage = document.querySelector('.displayed-img')
+var thumbBar = document.querySelector('.thumb-bar')
 
-btn = document.querySelector('button');
-var overlay = document.querySelector('.overlay');
+btn = document.querySelector('button')
+var overlay = document.querySelector('.overlay')
 
 /* Looping through images */
-
-  var newImage = document.createElement('img');
-  newImage.setAttribute('src', xxx);
-  thumbBar.appendChild(newImage);
+for (var i = 1; i < 6; i++) {
+  // wrong because https://wesbos.com/for-of-es6/
+  var newImage = document.createElement('img')
+  newImage.path = 'images/pic' + i + '.jpg'
+  newImage.setAttribute('src', newImage.path)
+  newImage.onclick = function () {
+    displayedImage.src = newImage.path
+  }
+  thumbBar.appendChild(newImage)
+}
 
 /* Wiring up the Darken/Lighten button */
-
