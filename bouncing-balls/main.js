0f7956c1447d07fbc5730ee296695d955466452a
Apostolos Sotiropoulos <apostolis@theartwishlist.com>
2018-06-05 14:40:32 +0300

    bouncing balls

--- bouncing-balls/main.js
+++ bouncing-balls/main.js
@@ -12,3 +12,71 @@ function random (min, max) {
   var num = Math.floor(Math.random() * (max - min)) + min
   return num
 }
+
+function Ball (x, y, velX, velY, color, size) {
+  this.x = x
+  this.y = y
+  this.velX = velX
+  this.velY = velY
+  this.color = color
+  this.size = size
+}
+
+Ball.prototype.draw = function () {
+  ctx.beginPath()
+  ctx.fillStyle = this.color
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI)
+  ctx.fill()
+}
+
+Ball.prototype.update = function () {
+  if ((this.x + this.size) >= width) {
+    this.velX = -(this.velX)
+  }
+
+  if ((this.x - this.size) <= 0) {
+    this.velX = -(this.velX)
+  }
+
+  if ((this.y + this.size) >= height) {
+    this.velY = -(this.velY)
+  }
+
+  if ((this.y - this.size) <= 0) {
+    this.velY = -(this.velY)
+  }
+
+  this.x += this.velX
+  this.y += this.velY
+}
+
+var balls = []
+
+while (balls.length < 2) {
+  var size = random(10, 50)
+  var ball = new Ball(
+    // ball position always drawn at least one ball width
+    // away from the adge of the canvas, to avoid drawing errors
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-7, 7),
+    random(-7, 7),
+    'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')',
+    size
+  )
+  balls.push(ball)
+}
+
+function loop () {
+  ctx.fillStyle = 'rgba(1, 2, 3, 0.5)'
+  ctx.fillRect(0, 0, width, height)
+
+  for (var i = 0; i < balls.length; i++) {
+    balls[i].draw()
+    balls[i].update()
+  }
+
+  requestAnimationFrame(loop)
+}
+
+loop()
