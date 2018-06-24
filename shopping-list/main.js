ac5df1a17b0c50069459c95843e8ba5c1209561b
Apostolos Sotiropoulos <apostolis@theartwishlist.com>
2018-06-24 19:38:17 +0300

    shoppling list: readability - removing grandparent

--- shopping-list/main.js
+++ shopping-list/main.js
@@ -6,12 +6,12 @@ function main () {
     remover.innerText = 'Remove'
     $(remover).on('click', removeFromList)
     newItem.appendChild(remover)
-    document.querySelector('#list').appendChild(newItem)
+    document.querySelector('ul').appendChild(newItem)
     document.querySelector('input').value = ''
   }
 
   function removeFromList (e) {
-    e.target.parentNode.parentNode.removeChild(e.target.parentNode)
+    document.querySelector('ul').removeChild(e.target.parentNode)
   }
 
   $('#adder').on('click', addToList)
